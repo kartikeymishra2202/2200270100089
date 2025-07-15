0a5c086c04b6184e03c06b10953bbf7cb6eb5887
kartikeymishra2202 <mharmilap@gmail.com>
2025-07-15 12:02:37 +0530

    second commit

--- Backend_Test_Sumission/urlShortener/config/db.js
+++ Backend_Test_Sumission/urlShortener/config/db.js
@@ -1,13 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI_;
-
 export async function connectDB() {
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error(" MongoDB connection error:", error);
